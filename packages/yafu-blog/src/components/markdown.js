b4eaa9b1bc20ac48e8fe02e09052f9719da3592a
Max Fierro <elmasse@gmail.com>
2020-07-01 18:28:19 -0300

    tertminate worker after message received.

--- packages/yafu-blog/src/components/markdown.js
+++ packages/yafu-blog/src/components/markdown.js
@@ -3,25 +3,22 @@ import Worker from '@geut/hast-worker'
 
 import { getConfig } from '../config'
 
-export default function Markdown({ path, children }) {
+export default function Markdown({ path, placeholder = null, children }) {
   const [post, setPost] = useState()
 
   useEffect(() => {
-    let worker
     (async () => {
       const { plugins: { rehype, remark } } = await getConfig()
       const raw = await beaker.hyperdrive.readFile(path)
-      worker = new Worker()
+      const worker = new Worker()
       worker.onmessage = function (event) {          
         setPost(event.data)
+        worker.terminate()
       };
       worker.postMessage({ raw, data: { path }, config: { rehype, remark } })
     })()
 
-    return () => {
-      worker && worker.terminate()
-    }
   }, [path])
 
-  return ( post ? children(post) : null )
+  return ( post ? children(post) : placeholder )
 }
